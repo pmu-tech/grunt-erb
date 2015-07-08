ad9003b5ac0c26db7b39d456d718d7217c0f70ec
Damien Camus <damien@sumac.fr>
2015-07-08 20:24:02 +0200

    Remove unused sinonjs

--- test/lib/erb.js
+++ test/lib/erb.js
@@ -1,6 +1,4 @@
 var assert = require('chai').assert;
-var sinon = require('sinon');
-
 var exec = require('child_process').exec;
 var sys = require('sys');
 
@@ -8,23 +6,23 @@ var erb = require('../../lib/erb');
 
 describe('lib/erb', function () {
 	describe('#handleCmdOuput', function () {
-		it('should throw an error when no ruby file path is specified', sinon.test(function () {
+		it('should throw an error when no ruby file path is specified', function () {
 			assert.throws(function () {
 				erb.compileTemplate()
 			}, 'No config file specified');
-		}));
+		});
 
-		it('should throw an error when no template file path is specified', sinon.test(function () {
+		it('should throw an error when no template file path is specified', function () {
 			assert.throws(function () {
 				erb.compileTemplate('file.rb')
 			}, 'No template specified file specified');
-		}));
+		});
 
-		it('should throw an error when no destination file path is specified', sinon.test(function () {
+		it('should throw an error when no destination file path is specified', function () {
 			assert.throws(function () {
 				erb.compileTemplate('file.rb', 'template.erb')
 			}, 'No destination specified');
-		}));
+		});
 
 		// TODO : add a test when every parameters are correct
 	});
--- test/util/string.js
+++ test/util/string.js
@@ -1,14 +1,12 @@
 var assert = require('chai').assert;
-var sinon = require('sinon');
-
 var string = require('../../util/string');
 
 describe('util/string', function () {
 	describe('#isEmpty', function () {
-		it('should return true', sinon.test(function () {
+		it('should return true', function () {
 			assert(true, string.isEmpty());
 			assert(true, string.isEmpty(''));
 			assert(true, string.isEmpty('       '));
-		}));
+		});
 	});
 });
